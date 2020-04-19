caeaa9a3c6391cb04bb0f6bff3805af550e6e16d
Francisco <poli.ferreras@gmail.com>
2020-04-19 22:29:31 +0200

    Listado de usuarios (sin acciones)

---
+++ ReGOLsivo/frontend/src/components/Aplicacion/ListUsuario.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div class="container">
+    <div class="row">
+      <div class="col text-left">
+        <h2>Listado de usuarios</h2>
+
+        <div class="col-md-12">
+          <b-table striped hover :items="usuarios" :fields="fields">
+          </b-table>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      fields: [
+        { key: 'Nombre', label: 'Nombre' },
+        { key: 'Apellidos', label: 'Apellidos' },
+        { key: 'Email', label: 'Email' },
+        { key: 'Karma', label: 'Karma' },
+        { key: 'action', label: 'Acción' }
+      ],
+      usuarios: []
+    }
+  },
+  methods: {
+
+    getUsuarios (){
+
+      const path = 'http://localhost:8000/api/v1.0/usuarios/'
+      axios.get(path).then((response) => {
+        this.usuarios = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+  },
+
+  created(){
+    this.getUsuarios()
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
