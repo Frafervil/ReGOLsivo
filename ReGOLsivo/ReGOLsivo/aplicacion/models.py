b1438e7e295b1cac7e2e12b97d91504d8c6aea1e
Francisco <poli.ferreras@gmail.com>
2020-04-24 01:58:35 +0200

    Modelo CRUD funcional

--- ReGOLsivo/ReGOLsivo/aplicacion/models.py
+++ ReGOLsivo/ReGOLsivo/aplicacion/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -31,13 +32,18 @@ class Administrador(Actor):
     def __str__(self):
         return super().nombre
 
+class Dificultad(models.TextChoices):
+    Facil = 'Facil'
+    Intermedia = 'Intermedia'
+    Dificil = 'Dificil'
+
 class Partido(models.Model):
     nombreLocal = models.CharField(max_length=35, null=False, blank=False)
     nombreVisitante = models.CharField(max_length=35, null=False, blank=False)
     resultado = models.CharField(max_length=35, null=False, blank=False)
     pronosticoSistema = models.CharField(max_length=35, null=False, blank=False)
     premio = models.PositiveIntegerField(null=False, blank=False)
-    dificultad = models.CharField(max_length=35, null=False, blank=False)
+    dificultad = models.CharField(max_length=35, choices=Dificultad.choices, null=False, blank=False)
 
     def __str__(self):
         cadena = "{0} - {1}"
@@ -50,20 +56,20 @@ class Pronostico(models.Model):
     partido = models.ForeignKey(Partido, null=False, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        cadena = "{2} => {3}"
-        return cadena.format(self.usuario, self.partido)
+        return self.usuario.cuentaDeUsuario.nombreDeUsuario, self.partido
 
 class Comentario(models.Model):
-    autor = models.CharField(max_length=35, null=False, blank=False)
     momento = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    texto = models.CharField(max_length=35, null=False, blank=False)
-    meGustas = models.PositiveIntegerField(null=False, blank=False)
-    #actor = models.ForeignKey(Actor, null=False, blank=True, on_delete=models.CASCADE)
-    partido = models.ForeignKey(Partido, null=False, blank=True, on_delete=models.CASCADE)
+    texto = models.TextField(null=False, blank=False)
+    meGustas = models.PositiveIntegerField(default=0, null=False, blank=False)
+    autor = models.ForeignKey(Usuario, null=False, on_delete=models.CASCADE)
+    partido = models.ForeignKey(Partido, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.autor.cuentaDeUsuario.nombreDeUsuario, self.partido
 
 class Configuracion(models.Model):
-    mensajeBienvenidaIn = models.CharField(max_length=60, null=False)
-    mensajeBienvenidaEs = models.CharField(max_length=60, null=False)
+    mensajeBienvenida = models.TextField(max_length=60, null=False)
     linkLogo = models.URLField(max_length=200, null=False)
     valorComentariosPositivos = models.PositiveIntegerField(default=50, null=False, blank=False)
-    valorComentariosPositivos = models.PositiveIntegerField(default=100, null=False, blank=False)
+    premioComentariosPositivos = models.PositiveIntegerField(default=100, null=False, blank=False)
